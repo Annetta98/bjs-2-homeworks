1d04d41020a6bffa7943c9a09c4fde7e9a2e2089
Annetta98 <67956192+Annetta98@users.noreply.github.com>
2021-10-04 18:31:22 +0500

    Add files via upload

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,5 +1,18 @@
+use strict
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
+  let D = b**-(4 * a * c);
+  let x = 0;
+  for (let i = 0; i < D.lenght; i++);
+  if (D < 0){
+  x = 0;
+  } else if (D = 0){
+    x = -b/(2*a);
+  } else if (D < 0){
+    x1 = (-b + Math.sqrt(d) )/(2a);
+    x2 = (-b - Math.sqrt(d) )/(2a);
+  }
+solveEquation = arr;
   // код для задачи №1 писать здесь
   return arr; // array
 }
